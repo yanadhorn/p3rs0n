4b76623866483a5873e47606163964bef284c1be
Pisit Supsri <p.supsri@gmail.com>
2017-12-12 15:30:17 +0700

    init project

---
+++ src/pages/Index.vue
@@ -0,0 +1,13 @@
+<template>
+<hello-world></hello-world>
+</template>
+
+<script>
+import HelloWorld from '../components/HelloWorld'
+
+export default {
+  components: {
+    HelloWorld
+  }
+}
+</script>
